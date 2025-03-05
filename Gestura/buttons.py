4ae3a0d541a61e613156a23bba8ea1001c0b43cb
Maryflor Campued <22-02371@g.batstate-u.edu.ph>
2025-03-05 22:43:37 +0800

    fixed window size and buttons positions

--- Gestura/buttons.py
+++ Gestura/buttons.py
@@ -6,29 +6,31 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Basic Sign Language Translator")
+        self.root.geometry("600x400")
+        self.root.resizable(False, False)
 
-        # Create a label for the video feed
         self.video_label = tk.Label(root)
         self.video_label.pack()
 
-        # buttons
-        self.start_button = tk.Button(root, text="Start", font=("Arial", 16), command=self.start_video)
-        self.learn_button = tk.Button(root, text="Learn", font=("Arial", 16), command=self.learn)
-        self.start_button.pack(side=tk.LEFT, padx=10, pady=10)
-        self.learn_button.pack(side=tk.RIGHT, padx=10, pady=10)
+        button_frame = tk.Frame(root)
+        button_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+        self.start_button = tk.Button(button_frame, text="Start", font=("Arial", 15), width=10, command=self.start_video)
+        self.learn_button = tk.Button(button_frame, text="Learn", font=("Arial", 15), width=10, command=self.learn)
+       
+        self.start_button.pack(side=tk.LEFT, padx=10)
+        self.learn_button.pack(side=tk.LEFT, padx=10)
 
         # Initialize the hand tracking generator
         self.hand_tracking_gen = None
 
     def start_video(self):
-        # Start the hand tracking generator
         self.hand_tracking_gen = hand_tracking()
         self.update_frame()
 
     def update_frame(self):
         if self.hand_tracking_gen:
             try:
-                # Get the next frame from the generator
                 frame = next(self.hand_tracking_gen)
 
                 img = Image.fromarray(frame)
@@ -45,7 +47,6 @@ class App:
         print("Learn button clicked")
 
     def on_closing(self):
-        # Clean up resources when the window is closed
         if self.hand_tracking_gen:
             self.hand_tracking_gen.close()  
         self.root.destroy()
